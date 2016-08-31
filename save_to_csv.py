57dea2eb31ecda557e813d9b5ad9c902db53ba2b
Andy Lulham <a.lulham@gmail.com>
2016-08-31 11:33:24 +0100

    Specify the column ordering

--- save_to_csv.py
+++ save_to_csv.py
@@ -14,6 +14,17 @@ entities = {
   "secretaries": "Secretary",
 }
 
+def gen_fieldnames(key_formats, all_fields):
+    fields = []
+    count = 1
+    while True:
+        if key_formats[0].format(count) not in all_fields:
+            break
+        for key_format in key_formats:
+            fields.append(key_format.format(count))
+        count += 1
+    return fields
+
 tables = {}
 for table in all_tables:
     print("Fetching from table '{}' ...".format(table))
@@ -31,18 +42,26 @@ results = {row["Entity Number"]: row for row in tables["data"]}
 print("Flattening tables ...")
 for table, entity in entities.items():
     for row in tables[table]:
-        prefix = "{}{:02d}".format(entity, row["{} Number".format(entity)])
+        num = int(row["{} Number".format(entity)])
+        prefix = "{}{:03d}".format(entity, num)
         d = {"{}{}".format(prefix, k): v for k, v in row.items() if k not in ["{} Number".format(entity), "Entity Number"]}
         results[row["Entity Number"]] = dict(results[row["Entity Number"]].items() + d.items())
 
 results = results.values()
 
 print("Generating csv '{}'".format(output_filename))
-fieldnames = set()
+all_fields = set()
 for res in results:
-    fieldnames |= set(res.keys())
+    all_fields |= set(res.keys())
 
-fieldnames = sorted(fieldnames)
+fieldnames = ['Entity Number', 'Entity Name', 'Status', 'CoStart', 'CoDate', 'Main Business Sector', 'Applicant',]
+fieldnames += gen_fieldnames(['Prev Name{:02d}',], all_fields)
+fieldnames += gen_fieldnames(['RO{:02d}Address', 'RO{:02d}Start', 'RO{:02d}End',], all_fields)
+fieldnames += gen_fieldnames(['PO{:02d}Address', 'PO{:02d}Start', 'PO{:02d}End',], all_fields)
+fieldnames += gen_fieldnames(['Director{:03d}Name', 'Director{:03d}Residential Address', 'Director{:03d}Postal Address', 'Director{:03d}Nationality', 'Director{:03d}Start', 'Director{:03d}End',], all_fields)
+fieldnames += ['Total Shares']
+fieldnames += gen_fieldnames(['Shareholder{:03d}Name', 'Shareholder{:03d}Address', 'Shareholder{:03d}PostalAddress', 'Shareholder{:03d}Place of Incorporation', 'Shareholder{:03d}Start', 'Shareholder{:03d}End',], all_fields)
+fieldnames += gen_fieldnames(['Secretary{:03d}Name', 'Secretary{:03d}Address', 'Secretary{:03d}PostalAddress', 'Secretary{:03d}Nationality', 'Secretary{:03d}Start', 'Secretary{:03d}End',], all_fields)
 
 with open(output_filename, 'w') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
